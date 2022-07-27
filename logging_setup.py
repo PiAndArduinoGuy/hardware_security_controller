9c18e4812b8dcf378145b805680940b40eee5cd8
QuintinKrugerDVT <QKruger@jhb.dvt.co.za>
2022-07-27 19:10:54 +0200

    Added ability to define properties using environment variables, Dockerfile defined to create image of service, renamed logging_file_location variable

--- logging_setup.py
+++ logging_setup.py
@@ -1,7 +1,12 @@
 import logging
 
 class LoggingSetup():
-    def __init__(self, logging_file_location: str):
-        logging.basicConfig(filename=logging_file_location,
+    def __init__(self, logging_file_directory: str):
+        try:
+            LOGGER.info(f"Creating logging directory {logging_file_directory}")
+            os.makedirs(os.getcwd() + f"/{logging_file_directory}")
+        except FileExistsError:
+            LOGGER.info(f"Logging directory {logging_file_directory} already exists.")
+        logging.basicConfig(filename=os.getcwd() + logging_file_directory + '/hardware-security-controller.log',
                             level=logging.INFO,
                             format='%(asctime)s:%(levelname)s:%(module)s:%(funcName)s(%(threadName)s)[%(lineno)d]:%(message)s')
